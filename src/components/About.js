83c8b2f7e84204ba9e8699b38516c5285fb405f4
Dharmil Shah <dharmilshah476@gmail.com>
2022-12-30 15:07:59 +0530

    Added text in about

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>Welcome to About</div>
-  )
+    <div>
+      <h3 className="my-3">
+        Parshwa Enterprises Pvt Ltd, is a leading supplier of infrared technology & industrial automation products in
+        India. Our products find wide application in sectors such as
+        manufacturing, construction, energy efficiency etc. With our in-house
+        research & development facilities, we customize our products to meet
+        specific client requirements. Our pan India sales & service network
+        ensures round the clock support and easy installation. We invite you to
+        explore our wide range of products.
+      </h3>
+    </div>
+  );
 }
 
-export default About
+export default About;
