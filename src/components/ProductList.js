beb0559a4cb3ed73517642f199f4574b7f20041d
Dharmil Shah <dharmilshah476@gmail.com>
2022-12-30 14:10:34 +0530

    Added images in different way

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,31 +1,44 @@
-import Logo from "../images/logo.png";
 const productList = [
     {
         id: 1,
         name: 'Product 1',
         price: 100,
-        image: Logo,
+        image: require('../images/image.jpeg'),
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
     },
     {
         id: 2,
         name: 'Product 2',
         price: 200,
-        image: Logo,
+        image: require('../images/logo.png'),
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
     },
     {
         id: 3,
         name: 'Product 3',
         price: 300,
-        image: Logo,
+        image: require('../images/image.jpeg'),
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
     },
     {
         id: 4,
         name: 'Product 4',
         price: 400,
-        image: Logo,
+        image: require('../images/logo.png'),
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+    },
+    {
+        id: 5,
+        name: 'Product 5',
+        price: 400,
+        image: require('../images/image.jpeg'),
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
+    },
+    {
+        id: 6,
+        name: 'Product 6',
+        price: 400,
+        image: require('../images/logo.png'),
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
     },
 ];
